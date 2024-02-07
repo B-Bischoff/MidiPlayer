d88305868421218eb4fd6b66d8938f4ebbd9c3c3
bbischoff <bbischoff@witekio.com>
2024-02-07 15:00:51 +0100

    removed port audio

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@
 #include <chrono>
 #include <thread>
 #include <assert.h>
-#include <portaudio.h>
 #include <portmidi.h>
 
-#include <rtaudio/RtAudio.h>
+#include <RtAudio.h>
 
 #define VERBOSE false
 
@@ -29,7 +28,6 @@ struct RingBuffer {
 struct AudioProperties {
 	unsigned int sampleRate;
 	unsigned int channels;
-	//PaSampleFormat sampleFormat; // typedef to unsigned long
 };
 
 struct AudioData {
@@ -59,46 +57,6 @@ double ocs(float hertz, float time)
 	double t = sinl(freqToAngularVelocity(hertz) * time);
 	return t;
 }
-
-void printPaDeviceInfo(const PaDeviceInfo* deviceInfo)
-{
-	assert(deviceInfo != nullptr && "Invalid deviceInfo");
-
-	std::cout << "name : " << deviceInfo->name << std::endl;
-	std::cout << "max input / output channels : " << deviceInfo->maxInputChannels << " " << deviceInfo->maxOutputChannels << std::endl;
-	std::cout << "max default low input / ouput latency : " << deviceInfo->defaultLowInputLatency << " " << deviceInfo->defaultLowOutputLatency << std::endl;
-	std::cout << "default sample rate : " << deviceInfo->defaultSampleRate << std::endl;
-	std::cout << std::endl;
-}
-
-//static int paCallback(const void* inputBuffer, void* outputBuffer,
-//		unsigned long framesPerBuffer,
-//		const PaStreamCallbackTimeInfo* timeInfo,
-//		PaStreamCallbackFlags statusFlags,
-//		void* data)
-//{
-//	AudioData& audioData = *(AudioData*)data;
-//	RingBuffer& ringBuffer = audioData.ringBuffer;
-//	float* out = (float*)outputBuffer;
-//
-//	for(int i = 0; i < audioData.properties.sampleRate; i++)
-//	{
-//		ringBuffer.buffer[i] = 0.00005f * ocs(440, (float)i / 44100.0f);
-//	}
-//
-//	//std::cout << "frames per buffer : " << framesPerBuffer <<	std::endl;
-//	//std::cout << "time elapsed: " << timeInfo->inputBufferAdcTime << std::endl;
-//
-//	for (int i = 0; i < framesPerBuffer; i++)
-//	{
-//		*out++ = ringBuffer.buffer[ringBuffer.leftPhase];
-//		*out++ = ringBuffer.buffer[ringBuffer.rightPhase];
-//		ringBuffer.leftPhase = (ringBuffer.leftPhase + 1) % audioData.properties.sampleRate;
-//		ringBuffer.rightPhase = (ringBuffer.rightPhase + 1) % audioData.properties.sampleRate;
-//	}
-//
-//	return paContinue;
-//}
 
 void portmidi_sandbox()
 {
@@ -152,63 +110,6 @@ void portmidi_sandbox()
 	Pm_Terminate();
 }
 
-//void portaudio_sandbox(AudioData& audioData)
-//{
-//	if (Pa_Initialize() != paNoError)
-//		exitError("[ERROR] Port Audio : initialization failed");
-//
-//	int numDevices = Pa_GetDeviceCount();
-//
-//	for (int deviceIndex = 0; deviceIndex < numDevices; deviceIndex++)
-//	{
-//		const PaDeviceInfo* deviceInfo = Pa_GetDeviceInfo(deviceIndex);
-//#if VERBOSE
-//		std::cout << "Device id : " << deviceIndex << std::endl;
-//		printPaDeviceInfo(deviceInfo);
-//#endif
-//	}
-//
-//	int defaultDevice = Pa_GetDefaultOutputDevice();
-//	if (defaultDevice == paNoDevice)
-//		exitError("[ERROR] Port Audio : no default output device found" );
-//
-//	std::cout << "(default) Device id : " << defaultDevice << std::endl;
-//	printPaDeviceInfo(Pa_GetDeviceInfo(defaultDevice));
-//
-//	PaStreamParameters outputParameters;
-//	outputParameters.device = defaultDevice;
-//	outputParameters.channelCount = audioData.properties.channels;
-//	outputParameters.sampleFormat = audioData.properties.sampleFormat;
-//	outputParameters.suggestedLatency = Pa_GetDeviceInfo(defaultDevice)->defaultLowOutputLatency; // [TODO] This parameter makes the program CPU hogging, check if this can be improved
-//	outputParameters.hostApiSpecificStreamInfo = nullptr;
-//
-//	// Allocate buffer for a duration of one second
-//	audioData.ringBuffer.buffer = nullptr;
-//	audioData.ringBuffer.buffer = new float[audioData.properties.sampleRate];
-//	if (audioData.ringBuffer.buffer == nullptr)
-//		exitError("[ERROR] could not allocate audio buffer");
-//
-//	PaStream* stream = nullptr;
-//	PaError error = Pa_OpenStream(
-//		&stream,
-//		nullptr,
-//		&outputParameters,
-//		audioData.properties.sampleRate,
-//		paFramesPerBufferUnspecified,
-//		paClipOff, // we won't output out of range samples so don't bother clipping them
-//		paCallback,
-//		&audioData // user data transfered to callback
-//	);
-//
-//	if (error != paNoError)
-//		exitError("[ERROR] Port Audio : could not open stream");
-//	if (Pa_StartStream(stream) != paNoError)
-//		exitError("[ERROR] Port Audio : could not start stream");
-//
-//	audioData.ringBuffer.stream = stream;
-//}
-
-// Two-channel sawtooth wave generator.
 int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
 				 double streamTime, RtAudioStreamStatus status, void *userData )
 {
@@ -234,14 +135,14 @@ int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
 		*buffer++ = rb.buffer[rb.rightPhase];
 		audio.incrementPhases();
 	}
-	//std::cout << rb.leftPhase / 2 << " " << rb.writeCursor / 2.0f << std::endl;
+	std::cout << rb.leftPhase / 2 << " " << rb.writeCursor / 2.0f << std::endl;
 
 	return 0;
 }
 
 int main(void)
 {
-	const unsigned int targetFPS = 441;
+	const unsigned int targetFPS = 60;
 	const std::chrono::duration<double> targetFrameDuration(1.0f / (double)targetFPS);
 
 	AudioData audio = {};
@@ -285,29 +186,14 @@ int main(void)
 		std::cout << dac.getErrorText() << std::endl;
 		exit(1);
 	}
-	/*
-	char input;
-	std::cout << "\nPlaying ... press <enter> to quit.\n";
-	std::cin.get( input );
-	// Block released ... stop the stream
-	if ( dac.isStreamRunning() )
-		dac.stopStream();	// or could call dac.abortStream();
- cleanup:
-	if ( dac.isStreamOpen() ) dac.closeStream();
-	return 0;
-	*/
 
 	//portmidi_sandbox();
 
 	//std::thread midiThread(portmidi_sandbox);
-	//Pa_Sleep(2 * 1000);
-
-	//portaudio_sandbox(audio);
-
 
 	auto programStartTime = std::chrono::high_resolution_clock::now();
 
-	rb.writeCursor = (44100.0f / (double)targetFPS) * 15.0f;
+	rb.writeCursor = (44100.0f / (double)targetFPS) * 35.0f;
 
 	rb.leftPhase = 0;
 	rb.rightPhase = 1;
@@ -316,7 +202,7 @@ int main(void)
 		auto startTime = std::chrono::high_resolution_clock::now();
 
 		auto programElapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(startTime - programStartTime);
-		std::cout << programElapsedTime.count() << std::endl;
+		//std::cout << programElapsedTime.count() << std::endl;
 
 		for (int i = 0; i < audio.properties.sampleRate / targetFPS; i++)
 		{
@@ -331,7 +217,6 @@ int main(void)
 			}
 		}
 		//std::cout << "wrote " << rb.writeCursor << std::endl;
-
 
 		auto endTime = std::chrono::high_resolution_clock::now();
 		auto deltaTime = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - startTime);
